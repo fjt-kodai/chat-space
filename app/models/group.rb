c82c79603e88c4ecfdcf5028f15831f1f12c477e
fjt-kodai <fjt.kodai@gmail.com>
2020-01-15 17:09:26 +0900

    Update model

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,23 @@
 class Group < ApplicationRecord
   has_many :group_users
   has_many :users, through: :group_users
+  has_many :messages
+
   validates :name, presence: true, uniqueness: true
+
+  def show_latest_message
+    if (latest_message = messages.last).present?
+
+      if latest_message.body?
+        latest_message.body
+      else
+        '画像が投稿されています。'
+      end
+
+    else
+      'まだメッセージはありません。'
+    end
+
+  end
+
 end
